b413caafc644d71c1a65e9e2a1cfb6ab5476974d
qkqhz <qkqhz@DESKTOP-6VMTT9C>
2022-02-21 14:16:19 +0900

    array solution for maximum days of the month

--- src/soohwan/calendar/Calendar.java
+++ src/soohwan/calendar/Calendar.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("일  월  화  수 목  금 토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+//		System.out.println("일  월  화  수 목  금 토");
+//		System.out.println("--------------------");
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		
+		/* 조건문을 활용한 시도.. 실패
 		String[] thirtymonth = {"4, 6, 9, 11"};
 		String[] thirtyonemonth = {"1, 3, 5, 7, 8, 10, 12"};
 		String elsemonth = "2";
@@ -28,5 +30,13 @@ public class Calendar {
 		} else if(elsemonth.equals(num)) {
 			System.out.println("28");
 		}
+		*/
+	
+		//배열을 활용하여 해결
+		int[] MaxMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.println("원하는 달을 입력하시면 최대 일 수를 알려드립니다.");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		System.out.printf("%d월의 최대 일 수는 %d입니다.", month, MaxMonth[month - 1]);
 	}
 }
